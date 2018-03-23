190eddd6ef8fdabaf7dce557df2c696b33af9d1f
GiangNV <nguyen.van.giang@framgia.com>
2018-03-23 10:52:53 +0700

    Create view layout

---
+++ resources/views/layouts/includes/script.blade.php
@@ -0,0 +1,7 @@
+<!-- jQuery Plugins -->
+<script type="text/javascript" src="plugins/creative-agency/js/jquery.min.js"></script>
+<script type="text/javascript" src="plugins/creative-agency/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="plugins/creative-agency/js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="plugins/creative-agency/js/jquery.magnific-popup.js"></script>
+<script type="text/javascript" src="plugins/creative-agency/js/main.js"></script>
+@yield('script')
---
+++ resources/views/layouts/includes/style.blade.php
@@ -0,0 +1,20 @@
+<!-- Google font -->
+{!! Html::style('https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round') !!}
+
+<!-- Bootstrap -->
+{!! Html::style('plugins/creative-agency/css/bootstrap.min.css') !!}
+
+<!-- Owl Carousel -->
+{!! Html::style('plugins/creative-agency/css/owl.carousel.css') !!}
+{!! Html::style('plugins/creative-agency/css/owl.theme.default.css') !!}
+
+<!-- Magnific Popup -->
+{!! Html::style('plugins/creative-agency/css/magnific-popup.css') !!}
+
+<!-- Font Awesome Icon -->
+{!! Html::style('plugins/creative-agency/css/font-awesome.min.css') !!}
+
+<!-- Custom stlylesheet -->
+{!! Html::style('plugins/creative-agency/css/style.css') !!}
+
+@yield('style')
