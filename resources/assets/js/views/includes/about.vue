559bd508d551cec3d5297893450251c0d129de83
thangtt <trungthangngo@gmail.com>
2018-03-27 11:11:33 +0700

    about, service component

--- resources/assets/js/views/includes/about.vue
+++ resources/assets/js/views/includes/about.vue
@@ -1,68 +1,38 @@
 <template>
-
-<!-- About -->
 <div id="about" class="section md-padding">
-
-    <!-- Container -->
     <div class="container">
-
-        <!-- Row -->
         <div class="row">
-
-            <!-- Section header -->
             <div class="section-header text-center">
                 <h2 class="title">Welcome to Website</h2>
             </div>
-            <!-- /Section header -->
-
-            <!-- about -->
-            <div class="col-md-4">
+            <div class="col-md-4" v-for="about in aboutList ">
                 <div class="about">
-                    <i class="fa fa-cogs"></i>
-                    <h3>Fully Customizible</h3>
-                    <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper libero sit amet.</p>
+                    <i :class="about.icon"></i>
+                    <h3>{{ about.title }}</h3>
+                    <p>{{ about.short_description }}</p>
                     <a href="#">Read more</a>
                 </div>
             </div>
-            <!-- /about -->
-
-            <!-- about -->
-            <div class="col-md-4">
-                <div class="about">
-                    <i class="fa fa-magic"></i>
-                    <h3>Awesome Features</h3>
-                    <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper libero sit amet.</p>
-                    <a href="#">Read more</a>
-                </div>
-            </div>
-            <!-- /about -->
-
-            <!-- about -->
-            <div class="col-md-4">
-                <div class="about">
-                    <i class="fa fa-mobile"></i>
-                    <h3>Fully Responsive</h3>
-                    <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper libero sit amet.</p>
-                    <a href="#">Read more</a>
-                </div>
-            </div>
-            <!-- /about -->
-
         </div>
-        <!-- /Row -->
-
     </div>
-    <!-- /Container -->
-
 </div>
-<!-- /About -->
-
 </template>
 
 <script>
     export default {
+        props: {
+            aboutList: {
+                required: true,
+                default: () => []
+            }
+        },
         mounted() {
-            console.log('about component.')
+            //
+        },
+        methods: {
+           //
         }
     }
 </script>
+
+
